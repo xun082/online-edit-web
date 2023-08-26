887824fe0d46fb53c50e217e7dc8a54a202e45bb
xun <2042204285@qq.com>
2023-08-27 07:35:56 +0800

    feat: 🚀 edit page new loading effect.

--- src/pages/edit/index.tsx
+++ src/pages/edit/index.tsx
@@ -2,6 +2,7 @@ import React, { FC, useState, useLayoutEffect, useEffect } from "react";
 import { PanelGroup, Panel } from "react-resizable-panels";
 import { WebContainer } from "@webcontainer/api";
 import { v4 as uuid } from "uuid";
+import { Alert, Spin } from "antd";
 
 import styles from "./index.module.scss";
 import { components, editorAside, ResizeHandle } from "./component";
@@ -92,67 +93,69 @@ const Edit: FC = () => {
   const Component = components[activeIcon];
 
   return (
-    <WebContainerContext.Provider
-      value={webContainerInstance && webContainerInstance}
-    >
-      <main className={styles["root"]}>
-        <nav className={styles["edit-nav"]}>
-          {editorAside.map(({ icon, label }) => (
-            <div
-              className={styles["aside-button"]}
-              key={label}
-              onClick={() => handleIconClick(label)}
-            >
-              {React.cloneElement(icon, {
-                style: {
-                  fontSize: "24px",
-                  color: activeIcon === label ? "#f0f0f0" : "#828388",
-                },
-              })}
-            </div>
-          ))}
-        </nav>
-        <section className={styles["edit-content"]}>
-          <PanelGroup direction="horizontal">
-            {/* 文件栏 */}
-            <Panel
-              style={{ background: "#202327" }}
-              minSize={1}
-              defaultSize={15}
-            >
-              <Component />
-            </Panel>
-            <ResizeHandle />
-            {/* 代码编辑栏 */}
-            <Panel minSize={1} defaultSize={previewSwitch !== true ? 50 : 85}>
-              <PanelGroup direction="vertical">
-                <Panel collapsible={true}>
-                  <div className={styles["edit-header"]}>1</div>
-                  <Editor filePath={path} />
-                </Panel>
-                <ResizeHandle direction="vertical" />
-                <Panel
-                  defaultSize={30}
-                  minSize={2}
-                  style={{ background: "#15181E" }}
-                >
-                  <TerminalPanel />
-                </Panel>
-              </PanelGroup>
-            </Panel>
-            {previewSwitch !== true ? (
-              <>
-                <ResizeHandle />
-                {/* 效果展示 */}
-                <Panel minSize={1} defaultSize={35}>
-                  <Preview data={linkData} />
-                </Panel>
-              </>
-            ) : null}
-          </PanelGroup>
-        </section>
-      </main>
-    </WebContainerContext.Provider>
+    <Spin spinning={webContainerInstance ? false : true}>
+      <WebContainerContext.Provider
+        value={webContainerInstance && webContainerInstance}
+      >
+        <main className={styles["root"]}>
+          <nav className={styles["edit-nav"]}>
+            {editorAside.map(({ icon, label }) => (
+              <div
+                className={styles["aside-button"]}
+                key={label}
+                onClick={() => handleIconClick(label)}
+              >
+                {React.cloneElement(icon, {
+                  style: {
+                    fontSize: "24px",
+                    color: activeIcon === label ? "#f0f0f0" : "#828388",
+                  },
+                })}
+              </div>
+            ))}
+          </nav>
+          <section className={styles["edit-content"]}>
+            <PanelGroup direction="horizontal">
+              {/* 文件栏 */}
+              <Panel
+                style={{ background: "#202327" }}
+                minSize={1}
+                defaultSize={15}
+              >
+                <Component />
+              </Panel>
+              <ResizeHandle />
+              {/* 代码编辑栏 */}
+              <Panel minSize={1} defaultSize={previewSwitch !== true ? 50 : 85}>
+                <PanelGroup direction="vertical">
+                  <Panel collapsible={true}>
+                    <div className={styles["edit-header"]}>1</div>
+                    <Editor filePath={path} />
+                  </Panel>
+                  <ResizeHandle direction="vertical" />
+                  <Panel
+                    defaultSize={30}
+                    minSize={2}
+                    style={{ background: "#15181E" }}
+                  >
+                    <TerminalPanel />
+                  </Panel>
+                </PanelGroup>
+              </Panel>
+              {previewSwitch !== true ? (
+                <>
+                  <ResizeHandle />
+                  {/* 效果展示 */}
+                  <Panel minSize={1} defaultSize={35}>
+                    <Preview data={linkData} />
+                  </Panel>
+                </>
+              ) : null}
+            </PanelGroup>
+          </section>
+        </main>
+      </WebContainerContext.Provider>
+    </Spin>
   );
 };
 
