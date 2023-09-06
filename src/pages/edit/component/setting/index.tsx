0e07d6cc30cd83085ac795079fed46f2c51558df
Eadweard <804875728@qq.com>
2023-09-06 14:46:49 +0800

    style: 🎨 prettier一些样式和细节优化

--- src/pages/edit/component/setting/index.tsx
+++ src/pages/edit/component/setting/index.tsx
@@ -1,8 +1,9 @@
-import React, { MouseEvent, useState, ChangeEvent } from "react";
+import React, { MouseEvent, useState, ChangeEvent, useRef } from "react";
 import { useSpring, animated } from "react-spring";
 import useMeasure from "react-use-measure";
-import { Input } from "antd";
+import { Input, type InputRef } from "antd";
 import { CloseOutlined, CheckOutlined } from "@ant-design/icons";
+import { Tooltip } from 'antd';
 
 import fileStyles from "../file/index.module.scss";
 
@@ -16,6 +17,7 @@ import { changeGlobalFileConfigPath } from "@/store/modules/code";
 const Setting = () => {
   const [isCollapsed, setIsCollapsed] = useState<boolean>(true);
   const [prettierPath, setPrettierPath] = useState<string>("");
+  const inputRef = useRef<InputRef | null>(null);
   const dispatch = useAppDispatch();
 
   const [ref, bounds] = useMeasure();
@@ -66,10 +68,18 @@ const Setting = () => {
       <div className={fileStyles['project-download']}>
         <h2 className={fileStyles['title']}>SETTINGS</h2>
       </div>
-      <div className={styles["switch-prettier-format"]} onClick={togglePanel}>
-        <div className={styles["format-title"]}>
-          根据项目中的prettier文件格式化代码
+      <div className={styles["prettier"]}>
+        <div className={styles["prettier-title"]}>
+          <div className={styles["icon"]}></div>
+          <h4>Prettier code</h4>
         </div>
+      </div>
+      <div className={styles["switch-prettier-format"]} onClick={togglePanel}>
+        <Tooltip title="根据项目中的prettier文件格式化代码">
+          <div className={styles["format-title"]}>
+            根据项目中的prettier文件格式化代码
+          </div>
+        </Tooltip>
         <animated.div
           style={panelContentAnimatedStyle}
           className={styles["content"]}
@@ -80,6 +90,7 @@ const Setting = () => {
             className={styles["file-path-control"]}
           >
             <Input
+              ref={inputRef}
               placeholder={
                 localStorage.getItem(PRETTIER_FORMAT_PATH)
                   ? (localStorage.getItem(PRETTIER_FORMAT_PATH) as string)
@@ -105,12 +116,15 @@ const Setting = () => {
           </div>
         </animated.div>
       </div>
-      <div
-        className={styles["global-format-title"]}
-        onClick={settingGlobalPrettierConfig}
-      >
-        全局的prettier文件格式化代码
-      </div>
+      <Tooltip title="全局的prettier文件格式化代码">
+        <div
+          className={styles["global-format-title"]}
+          onClick={settingGlobalPrettierConfig}
+        >
+          全局的prettier文件格式化代码
+        </div>
+      </Tooltip>
+
     </div>
   );
 };
