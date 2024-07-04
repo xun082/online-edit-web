bf1bca245ae215db40c50924242a6a383b6c87a9
Moment <73689580+xun082@users.noreply.github.com>
2024-07-04 23:50:32 +0800

    feat: 🚀 add next-devtool (#28)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { NextDevtoolsProvider } from '@next-devtools/core';
+
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NextDevtoolsProvider>{children}</NextDevtoolsProvider>
+      </body>
     </html>
   );
 }
