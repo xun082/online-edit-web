1fc7f227ca9b40a3b9ad19cb31a04c6ec960acef
Moment <73689580+xun082@users.noreply.github.com>
2024-07-06 16:35:38 +0800

    fix: 🧩 修复打包构建失败的问题 (#33)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,11 +17,11 @@ const config: Config = {
       keyframes: {
         spotlight: {
           '0%': {
-            opacity: 0,
+            opacity: '0',
             transform: 'translate(-72%, -62%) scale(0.5)',
           },
           '100%': {
-            opacity: 1,
+            opacity: '1',
             transform: 'translate(-50%,-40%) scale(1)',
           },
         },
