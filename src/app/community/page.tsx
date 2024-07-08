e397edd98e4c5d9fcea14ab291af0dba06f2f5dd
Moment <73689580+xun082@users.noreply.github.com>
2024-07-08 08:09:39 +0800

    fix: 🧩 use @giscus/react to configure and render the commenting system (#43)

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,42 +1,25 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React from 'react';
+import Giscus from '@giscus/react';
 
 const GiscusComments: React.FC = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://giscus.app/client.js';
-    script.setAttribute('data-repo', 'xun082/online-edit-web');
-    script.setAttribute('data-repo-id', 'R_kgDOJ4NYUQ');
-    script.setAttribute('data-category', 'Announcements');
-    script.setAttribute('data-category-id', 'DIC_kwDOJ4NYUc4Cgojg');
-    script.setAttribute('data-mapping', 'title');
-    script.setAttribute('data-strict', '0');
-    script.setAttribute('data-reactions-enabled', '1');
-    script.setAttribute('data-emit-metadata', '0');
-    script.setAttribute('data-input-position', 'top');
-    script.setAttribute('data-theme', 'dark');
-    script.setAttribute('data-lang', 'zh-CN');
-    script.setAttribute('data-loading', 'lazy');
-    script.setAttribute('crossorigin', 'anonymous');
-    script.async = true;
-
-    const comments = document.getElementById('giscus-comments');
-
-    if (comments) {
-      comments.appendChild(script);
-    }
-
-    return () => {
-      if (comments) {
-        comments.innerHTML = '';
-      }
-    };
-  }, []);
-
   return (
     <div className="max-w-4xl mx-auto p-4 bg-gray-800 rounded-lg shadow-lg">
-      <div id="giscus-comments" />
+      <Giscus
+        repo="xun082/online-edit-web"
+        repoId="R_kgDOJ4NYUQ"
+        category="Announcements"
+        categoryId="DIC_kwDOJ4NYUc4Cgojg"
+        mapping="title"
+        strict="0"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="top"
+        theme="dark"
+        lang="zh-CN"
+        loading="lazy"
+      />
     </div>
   );
 };
