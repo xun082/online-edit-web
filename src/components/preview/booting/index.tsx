ccaa4c2b590a2677e026386b964ff58132c02c73
Sparkle <139463884+6iKUN6@users.noreply.github.com>
2024-07-10 00:44:58 +0800

    style：Preview Component Style writing (#45)
    * pnpm-lock.yaml update
    
    * preview 组件样式调整
    
    * preview 组件样式调整
    
    * preview 组件样式调整
    
    * preview 组件样式调整
    
    * preview 组件样式调整
    
    * preview 组件样式调整
    
    ---------
    
    Co-authored-by: 社交大师富冈义勇 <9702605+sblwl@user.noreply.gitee.com>

---
+++ src/components/preview/booting/index.tsx
@@ -0,0 +1,66 @@
+import { FC, useState, ReactNode } from 'react';
+import { FaLightbulb, FaArrowsRotate, FaCircleCheck, FaO, FaCircleXmark } from 'react-icons/fa6';
+
+const statusIcon: Record<string, ReactNode> = {
+  '-1': <FaCircleXmark className="text-red-400" aria-label="Error" />,
+  '2': <FaCircleCheck className="text-green-500" aria-label="Success" />,
+  '1': (
+    <FaArrowsRotate
+      className="animate-spin text-blue-500 rotate-360 infinite duration-1000"
+      aria-label="Loading"
+    />
+  ),
+  '0': (
+    <span className="text-yellow-500" aria-label="Initializing">
+      <FaO />
+    </span>
+  ),
+};
+interface IStepQueue {
+  condition: string;
+  title: string;
+}
+
+const BootingStep: FC<IStepQueue> = ({ condition, title }) => {
+  const Icon = statusIcon[condition];
+
+  return (
+    <div className="flex items-center">
+      {Icon}
+      <span className="mx-2">{title}</span>
+    </div>
+  );
+};
+
+const stepQueue: Array<IStepQueue> = [
+  {
+    condition: '2',
+    title: 'Booting WebContainer',
+  },
+  {
+    condition: '1',
+    title: 'Installing dependencies',
+  },
+  {
+    condition: '0',
+    title: 'Running start command',
+  },
+];
+
+const BootingWebContainer: FC = () => {
+  const [steps] = useState<Array<IStepQueue>>(stepQueue);
+
+  return (
+    <div className="h-full w-full flex flex-col justify-center  items-center">
+      <FaLightbulb className={`${true ? 'text-sky-400' : 'text-gray-400'} text-8xl my-4`} />
+      <h2 className="mb-6">Installing dependencies</h2>
+      <div className="steps">
+        {steps.map((step, index) => (
+          <BootingStep key={index} {...step} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BootingWebContainer;
