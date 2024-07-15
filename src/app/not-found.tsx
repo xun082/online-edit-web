cb3cdc6401e5999fe5faccdbb0093d1e60bd3772
Sparkle <9702605+sblwl@user.noreply.gitee.com>
2024-07-15 17:46:52 +0800

    feat:add 404NotFound page component and route

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import type { NextPage } from 'next';
+
+import NotFoundSvgComp from '@/components/404';
+
+const NotFound: NextPage = () => {
+  return <NotFoundSvgComp />;
+};
+
+export default NotFound;
