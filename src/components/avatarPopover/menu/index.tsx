5e8d022cc732a9a56946ee27d2a631858ac523e3
Sparkle <9702605+sblwl@user.noreply.gitee.com>
2024-07-20 15:25:01 +0800

    feat: 🚀 增加settings页面和修改menu组件

--- src/components/avatarPopover/menu/index.tsx
+++ src/components/avatarPopover/menu/index.tsx
@@ -2,26 +2,33 @@
 
 import { FC, ReactNode } from 'react';
 
+import { cn } from '@/utils';
+
 const MenuItem: FC<{
   children?: ReactNode;
   className?: string;
   onClick?: React.MouseEventHandler<HTMLDivElement>;
 }> = ({ children, className, onClick }) => {
-  const defaultClassName = `w-full p-1 hover:bg-blue-500 transition-colors cursor-pointer`;
-
   return (
-    <div className={`${defaultClassName} ${className ?? ''}`} onClick={onClick}>
+    <div
+      className={cn('w-full p-1 hover:bg-blue-500 transition-colors cursor-pointer', className)}
+      onClick={onClick}
+    >
       {children}
     </div>
   );
 };
 
-const Menu: FC<{ children?: ReactNode; className?: string }> = ({ children, className }) => {
-  const defaultClassName = `relative w-full py-2 `;
+const Menu: FC<{
+  children?: ReactNode;
+  className?: string;
+  topLine?: boolean | undefined;
+}> = ({ children, className, topLine = false }) => {
+  const hasTopLine = topLine || (typeof topLine === 'undefined' && children);
 
   return (
-    <div className={`${defaultClassName} ${className ?? ''}`}>
-      <div className="w-full h-[1px] bg-gray-600 absolute top-0 opacity-50"></div>
+    <div className={cn('relative w-full py-2', className)}>
+      {hasTopLine && <div className="w-full h-[1px] bg-gray-600 absolute top-0 opacity-50"></div>}
       {children}
     </div>
   );
