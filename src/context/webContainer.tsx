a1f3409133cc7940bff9e3d45d56712a73ff5c61
xun082 <2042204285@qq.com>
2024-07-23 22:42:50 +0800

    wip: 添加 webContainer 一些基础配置

--- src/context/webContainer.tsx
+++ src/context/webContainer.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useRef, ReactNode } from 'react';
 import { WebContainer } from '@webcontainer/api';
 
 import { WebContainerFileSystemTreeSavePoint, saveFileSystemTree } from '@/utils';
@@ -9,34 +9,35 @@ interface WebContainerContextProps {
 
 const WebContainerContext = createContext<WebContainerContextProps | undefined>(undefined);
 
-export const WebContainerProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [webContainerInstance, setWebContainerInstance] = useState<WebContainer | null>(null);
+export const WebContainerProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const webContainerInstance = useRef<WebContainer | null>(null);
+  const isInitialized = useRef(false);
 
   useEffect(() => {
     const bootWebContainer = async () => {
-      const instance = await WebContainer.boot();
+      if (!isInitialized.current && !webContainerInstance.current) {
+        webContainerInstance.current = await WebContainer.boot();
 
-      const treeData = localStorage.getItem(WebContainerFileSystemTreeSavePoint);
+        const treeData = localStorage.getItem(WebContainerFileSystemTreeSavePoint);
 
-      if (treeData) {
-        const fileSystemTree = JSON.parse(treeData);
-        await instance.mount(fileSystemTree);
+        if (treeData) {
+          const fileSystemTree = JSON.parse(treeData);
+          await webContainerInstance.current.mount(fileSystemTree);
+        }
+
+        isInitialized.current = true;
       }
-
-      setWebContainerInstance(instance);
     };
 
-    if (!webContainerInstance) {
-      bootWebContainer();
-    }
-  }, [webContainerInstance]);
+    bootWebContainer();
+  }, []);
 
   useEffect(() => {
     const handleBeforeUnload = (event: BeforeUnloadEvent) => {
       event.preventDefault();
 
-      if (webContainerInstance) {
-        saveFileSystemTree(webContainerInstance);
+      if (webContainerInstance.current) {
+        saveFileSystemTree(webContainerInstance.current);
       }
     };
 
@@ -45,10 +46,10 @@ export const WebContainerProvider: React.FC<{ children: React.ReactNode }> = ({ 
     return () => {
       window.removeEventListener('beforeunload', handleBeforeUnload);
     };
-  }, [webContainerInstance]);
+  }, []);
 
   return (
-    <WebContainerContext.Provider value={{ webContainerInstance }}>
+    <WebContainerContext.Provider value={{ webContainerInstance: webContainerInstance.current }}>
       {children}
     </WebContainerContext.Provider>
   );
