a0f1cc97543d32bd8df5ecd07f741ea199f6ab20
zhwcreate <3331598351@qq.com>
2024-10-23 23:07:32 +0800

    feat: 完善协同选区,光标,头像效果

--- src/components/cooperation/cooperationEditor/index.tsx
+++ src/components/cooperation/cooperationEditor/index.tsx
@@ -1,20 +1,18 @@
 'use client';
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useMemo, useState } from 'react';
 import dynamic from 'next/dynamic';
 import * as monaco from 'monaco-editor';
-import { createHighlighter } from 'shiki';
-import { shikiToMonaco } from '@shikijs/monaco';
 import * as Y from 'yjs';
 import { WebsocketProvider } from 'y-websocket';
 import { MonacoBinding } from 'y-monaco';
-import { Monaco } from '@monaco-editor/react';
 
+import { Cursor } from '@/components/cooperation/cursor';
 import LoadingComponent from '@/components/edit/edit-loading';
 import { CooperationEditorHeader } from '@/components/cooperation/cooperationEditor/cooperationEditorHeader';
+import { useCooperationPerson } from '@/store/cooperationPersonStore';
+import { hexToRgba, throttle, createColorFromId } from '@/utils';
 
-// 动态导入 @monaco-editor/react，确保它只在客户端加载
 const Editor = dynamic(() => import('@monaco-editor/react'), { ssr: false });
 
 type CooperationEditorProps = {
@@ -25,7 +23,9 @@ export const CooperationEditor: React.FC<CooperationEditorProps> = ({ roomId }) 
   const ydoc = useMemo(() => new Y.Doc(), []);
   const [editor, setEditor] = useState<monaco.editor.IStandaloneCodeEditor | null>(null);
   const [provider, setProvider] = useState<WebsocketProvider | null>(null);
-  const [binding, setBinding] = useState<MonacoBinding | null>(null);
+  const [, setBinding] = useState<MonacoBinding | null>(null);
+  const [awareness, setAwareness] = useState<any[]>();
+  const { addPersons, removePersons } = useCooperationPerson();
 
   useEffect(() => {
     if (roomId == null) {
@@ -45,11 +45,16 @@ export const CooperationEditor: React.FC<CooperationEditorProps> = ({ roomId }) 
     };
   }, [ydoc, roomId]);
 
-  // this effect manages the lifetime of the editor binding
+  // This effect manages the lifetime of the editor binding
   useEffect(() => {
     if (provider == null || editor == null || roomId === null) {
       return;
     }
+
+    provider.awareness.setLocalStateField('cursorLocation', {
+      x: undefined,
+      y: undefined,
+    });
 
     const binding = new MonacoBinding(
       ydoc.getText(),
@@ -59,16 +64,92 @@ export const CooperationEditor: React.FC<CooperationEditorProps> = ({ roomId }) 
     );
     setBinding(binding);
 
+    const handleMouseMove = throttle((e: MouseEvent) => {
+      provider.awareness.setLocalStateField('cursorLocation', {
+        x: e.clientX,
+        y: e.clientY,
+      });
+    }, 15);
+
+    const handleMouseout = () => {
+      provider.awareness.setLocalStateField('cursorLocation', {
+        x: undefined,
+        y: undefined,
+      });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseout', handleMouseout);
+
+    // Create a single <style> element on mount
+    const styleElement = document.createElement('style');
+    document.head.appendChild(styleElement);
+
+    provider.awareness.on(
+      'change',
+      ({
+        updated,
+        added,
+        removed,
+      }: {
+        updated: Array<number>;
+        added: Array<number>;
+        removed: Array<number>;
+      }) => {
+        type UserAwarenessData = Map<number, { firebaseUserID: string; selection: any }>;
+
+        let awarenessState = provider.awareness.getStates() as UserAwarenessData;
+        setAwareness(Array.from(awarenessState));
+
+        let newStyles = '';
+        addPersons(added.filter((id) => id !== ydoc.clientID));
+        addPersons(updated.filter((id) => id !== ydoc.clientID));
+        removePersons(removed.filter((id) => id !== ydoc.clientID));
+
+        for (let addedUserID of updated) {
+          if (addedUserID === ydoc.clientID) return;
+
+          newStyles += `
+          .yRemoteSelection-${addedUserID} {
+            background-color: ${createColorFromId(addedUserID)};
+            color: ${createColorFromId(addedUserID)};
+          }
+          .yRemoteSelectionHead-${addedUserID} {
+            position: relative;
+            border-left: 2px solid ${createColorFromId(addedUserID)};
+            border-top: 2px solid ${createColorFromId(addedUserID)};
+            border-bottom: 2px solid ${createColorFromId(addedUserID)};
+            height: 100%;
+            box-sizing: border-box;
+          }
+          .yRemoteSelectionHead-${addedUserID}::after {
+            position: absolute;
+            content: "用户${addedUserID}";
+            color: white;
+            background-color: ${hexToRgba(createColorFromId(addedUserID), 0.5)};
+            padding: 4px;
+            margin: 4px;
+            font-size: 12px;
+            left: 0;
+            top: 100%;
+          }
+        `;
+        }
+
+        styleElement.innerHTML += newStyles;
+      },
+    );
+
     return () => {
       binding.destroy();
       setBinding(null);
+      styleElement.remove();
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseout', handleMouseout);
     };
   }, [ydoc, provider, editor, roomId]);
 
-  const handleEditorDidMount = async (
-    editor: monaco.editor.IStandaloneCodeEditor,
-    monaco: Monaco,
-  ) => {
+  const handleEditorDidMount = async (editor: monaco.editor.IStandaloneCodeEditor) => {
     setEditor(editor);
   };
 
@@ -97,6 +178,18 @@ export const CooperationEditor: React.FC<CooperationEditorProps> = ({ roomId }) 
           onChange={handleEditorChange}
           onMount={handleEditorDidMount}
         />
+        {awareness
+          ?.filter(([id]) => id !== ydoc.clientID) // Filter out self
+          .filter(([, state]) => state.cursorLocation.x !== undefined) // Filter out users who have left the page
+          .map(([id, state]) => {
+            return (
+              <Cursor
+                key={id}
+                color={createColorFromId(id)}
+                point={[state.cursorLocation.x, state.cursorLocation.y]}
+              ></Cursor>
+            );
+          })}
       </div>
     </div>
   );
---
+++ src/hooks/usePerfectCursor.ts
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { PerfectCursor } from 'perfect-cursors';
+
+export default function usePerfectCursor(cb: (point: number[]) => void, point?: number[]) {
+  const [pc] = React.useState(() => new PerfectCursor(cb));
+
+  React.useLayoutEffect(() => {
+    if (point) pc.addPoint(point);
+
+    return () => pc.dispose();
+  }, [pc]);
+
+  const onPointChange = React.useCallback((point: number[]) => pc.addPoint(point), [pc]);
+
+  return onPointChange;
+}
