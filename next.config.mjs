7a493d480afddd4e67455307b5825fad218426ba
zhwcreate <3331598351@qq.com>
2024-10-29 02:12:46 +0800

    feat: 还原nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,23 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
