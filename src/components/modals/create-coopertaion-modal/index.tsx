da11d3be3b6ae1cf586a7746b5e6becab2b4ac0d
zhw <116932167+coderz-w@users.noreply.github.com>
2024-10-31 00:31:00 +0800

    feat: 修复头像问题,调整鉴权页面,更新后端接口 (#139)
    * feat: 添加github链接
    
    * fix: 修复头像问题
    
    * feat: 修改鉴权页面
    
    * feat: 减少不必要请求
    
    * fix: 修复css

--- src/components/modals/create-coopertaion-modal/index.tsx
+++ src/components/modals/create-coopertaion-modal/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 import { Button } from '@/components/ui/button';
 import {
@@ -12,15 +12,36 @@ import {
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { useModal } from '@/hooks/useModal';
+import { useAuthStore } from '@/store/authStore';
+import { PATHS, STORAGE_KEY_AUTH } from '@/utils';
 
 export function CreateCoopertaionModal() {
+  const { setAuth, getAuth } = useAuthStore();
   const router = useRouter();
+  const pathname = usePathname();
   const [docName, setDocName] = useState('');
   const { isOpen, onClose, type } = useModal();
   const isModalOpen = isOpen && type === 'createCooperation';
 
   const handleCreate = async () => {
-    // 发送 POST 请求
+    let auth = getAuth();
+
+    if (!auth.access_token) {
+      const storagedAuth = localStorage.getItem(STORAGE_KEY_AUTH);
+
+      if (storagedAuth) {
+        auth = JSON.parse(storagedAuth);
+
+        setAuth(auth);
+      }
+    }
+
+    if (!auth.access_token) {
+      router.push(`${PATHS.LOGIN}?redirect=${pathname}`);
+
+      return;
+    }
+
     const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/v1/document/create`, {
       method: 'POST',
       headers: {
